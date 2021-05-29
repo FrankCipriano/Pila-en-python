c426a7f9e729cd9619fe589a05b3c72d8bf1753c
FrankCipriano <francoswordfish@gmail.com>
2021-05-28 21:08:49 -0600

    operaciones basicas con una pila en python

---
+++ pila.py
@@ -0,0 +1,13 @@
+#creando la pila
+pila = [1,2,3,4,"frank",5.7]
+print(pila)
+
+#apilando mas datos a la pila
+pila.append(6)
+pila.append(7)
+pila.append("cipriano")
+print(pila)
+
+#desapilando elementos de la pila
+pila.pop()
+print(pila)
